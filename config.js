79dd040a778ca5b0479c0e16665d54f21696e615
TheShurikUA <utubivskijsurik@gmail.com>
2026-07-27 10:44:38 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// ЗАПОВНИ ЦІ ДВА ПОЛЯ своїми даними після створення репозиторію на GitHub.
+// Приклад: якщо посилання на репозиторій https://github.com/ivan123/news-feed
+// то OWNER = "ivan123", REPO = "news-feed"
+const SITE_CONFIG = {
+  OWNER: "your-github-username",
+  REPO: "your-repo-name",
+  BRANCH: "main",
+};
