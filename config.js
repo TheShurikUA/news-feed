9fecb60f7d4291e39546049dadf8fc0b6c388f90
TheShurikUA <utubivskijsurik@gmail.com>
2026-07-27 10:47:48 +0300

    Update GitHub owner and repository name in config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Приклад: якщо посилання на репозиторій https://github.com/ivan123/news-feed
 // то OWNER = "ivan123", REPO = "news-feed"
 const SITE_CONFIG = {
-  OWNER: "your-github-username",
-  REPO: "your-repo-name",
+  OWNER: "TheShurikUA",
+  REPO: "news-feed",
   BRANCH: "main",
 };
